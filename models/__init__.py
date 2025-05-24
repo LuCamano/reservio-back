9f004b323c555efee14918a71c2cac8eb69e0613
Luciano <lu.camano@duocuc.cl>
2025-05-23 21:48:01 -0400

    Agregar modelo Usuario y endpoints para gestión de usuarios en la API

--- models/__init__.py
+++ models/__init__.py
@@ -1,25 +1,49 @@
 from sqlmodel import SQLModel, Field, Relationship
+import enum
+from sqlalchemy.types import Enum
 import uuid
 from typing import Optional
 from uuid import UUID
 from datetime import date
 
-class AutorBase(SQLModel):
-    nombre: str = Field(max_length=100, nullable=False)
-    apellido: str = Field(max_length=100, nullable=False)
-    nacimiento: date = Field(default=None)
+class UserType(enum.Enum):
+    """ Enumerador para el tipo de usuario """
+    admin = "admin"
+    cliente = "cliente"
+    propietario = "propietario"
+
+# class AutorBase(SQLModel):
+#     nombre: str = Field(max_length=100, nullable=False)
+#     apellido: str = Field(max_length=100, nullable=False)
+#     nacimiento: date = Field(default=None)
     
-class Autor(AutorBase, table=True):
-    __tablename__ = "autor"
+# class Autor(AutorBase, table=True):
+#     __tablename__ = "autor"
+#     id: Optional[UUID] = Field(default_factory=uuid.uuid4, primary_key=True)
+#     libros: list["Libro"] = Relationship(back_populates="autor")
+    
+# class LibroBase(SQLModel):
+#     titulo: str = Field(max_length=100, nullable=False)
+#     fecha_publicacion: date = Field(default=None)
+#     autor_id: Optional[UUID] = Field(default=None, foreign_key="autor.id")
+    
+# class Libro(LibroBase, table=True):
+#     __tablename__ = "libro"
+#     id: Optional[UUID] = Field(default_factory=uuid.uuid4, primary_key=True)
+#     autor: Optional[Autor] = Relationship(back_populates="libros")
+
+class UsuarioBase(SQLModel):
+    email: str = Field(max_length=150, nullable=False, unique=True, index=True)
+    rut: str = Field(max_length=12, nullable=False, unique=True, index=True)
+    nombres: str = Field(max_length=150, nullable=False)
+    appaterno: str = Field(max_length=100, nullable=False)
+    apmaterno: str = Field(max_length=100, nullable=False)
+    fecha_nacimiento: date = Field(nullable=False)
+    tipo: UserType = Field(default='cliente', sa_type=Enum(UserType))
+    password: str = Field(max_length=100, nullable=False)
+    
+class Usuario(UsuarioBase, table=True):
+    __tablename__ = "usuario"
     id: Optional[UUID] = Field(default_factory=uuid.uuid4, primary_key=True)
-    libros: list["Libro"] = Relationship(back_populates="autor")
-    
-class LibroBase(SQLModel):
-    titulo: str = Field(max_length=100, nullable=False)
-    fecha_publicacion: date = Field(default=None)
-    autor_id: Optional[UUID] = Field(default=None, foreign_key="autor.id")
-    
-class Libro(LibroBase, table=True):
-    __tablename__ = "libro"
-    id: Optional[UUID] = Field(default_factory=uuid.uuid4, primary_key=True)
-    autor: Optional[Autor] = Relationship(back_populates="libros")
+    fecha_creacion: date = Field(default=date.today())
+    activo: bool = Field(default=True)
--- views/__init__.py
+++ views/__init__.py
@@ -1,75 +1,99 @@
 from typing import Annotated
 from fastapi import APIRouter, HTTPException, Query
 from sqlmodel import select
+from models import (Usuario, UsuarioBase)
 from app.db import SessionDep
-from models import (Autor, AutorBase, Libro, LibroBase)
 from uuid import UUID
 router = APIRouter()
 
-@router.get("/")
-async def read_root():
-    return {"message": "Hola papus"}
-
-@router.get("/autores", response_model=list[Autor])
-async def read_autores(
+@router.get("/users", response_model=list[Usuario])
+async def read_users(
     session: SessionDep,
     offset: int = 0,
-    limit: Annotated[int, Query(le=100)] = 10,
+    limit: Annotated[int, Query(le=100)] = 10
 ):
-    autores = session.exec(select(Autor).offset(offset).limit(limit)).all()
-    if not autores:
-        raise HTTPException(status_code=404, detail="No se encontraron autores")
-    return autores
+    users = session.exec(select(Usuario).offset(offset).limit(limit)).all()
+    if not users:
+        raise HTTPException(status_code=404, detail="No se encontraron usuarios")
+    return users
 
-@router.post("/autores", response_model=Autor)
-async def create_autor(
-    autor: AutorBase,
+@router.post("/users", response_model=Usuario)
+async def create_user(
     session: SessionDep,
+    user: UsuarioBase
 ):
-    db_autor = Autor.model_validate(autor)
-    session.add(db_autor)
+    db_user = Usuario.model_validate(user)
+    session.add(db_user)
     session.commit()
-    session.refresh(db_autor)
-    return db_autor
+    session.refresh(db_user)
+    return db_user
 
-@router.get("/autores/{autor_id}", response_model=Autor)
-async def read_autor(
-    autor_id: UUID,
-    session: SessionDep,
-):
-    autor = session.get(Autor, autor_id)
-    if not autor:
-        raise HTTPException(status_code=404, detail="Autor no encontrado")
-    return autor
+# from models import (Autor, AutorBase, Libro, LibroBase)
 
-@router.post("/libros", response_model=Libro)
-async def create_libro(
-    libro: LibroBase,
-    session: SessionDep
-):
-    db_libro = Libro.model_validate(libro)
-    session.add(db_libro)
-    session.commit()
-    session.refresh(db_libro)
-    return db_libro
+# @router.get("/")
+# async def read_root():
+#     return {"message": "Hola papus"}
 
-@router.get("/libros", response_model=list[Libro])
-async def read_libros(
-    session: SessionDep,
-    offset: int = 0,
-    limit: Annotated[int, Query(le=100)] = 10,
-):
-    libros = session.exec(select(Libro).offset(offset).limit(limit)).all()
-    if not libros:
-        raise HTTPException(status_code=404, detail="No se encontraron libros")
-    return libros
+# @router.get("/autores", response_model=list[Autor])
+# async def read_autores(
+#     session: SessionDep,
+#     offset: int = 0,
+#     limit: Annotated[int, Query(le=100)] = 10,
+# ):
+#     autores = session.exec(select(Autor).offset(offset).limit(limit)).all()
+#     if not autores:
+#         raise HTTPException(status_code=404, detail="No se encontraron autores")
+#     return autores
 
-@router.get("/libros/{libro_id}", response_model=Libro)
-async def read_libro(
-    libro_id: UUID,
-    session: SessionDep,
-):
-    libro = session.get(Libro, libro_id)
-    if not libro:
-        raise HTTPException(status_code=404, detail="Libro no encontrado")
-    return libro
+# @router.post("/autores", response_model=Autor)
+# async def create_autor(
+#     autor: AutorBase,
+#     session: SessionDep,
+# ):
+#     db_autor = Autor.model_validate(autor)
+#     session.add(db_autor)
+#     session.commit()
+#     session.refresh(db_autor)
+#     return db_autor
+
+# @router.get("/autores/{autor_id}", response_model=Autor)
+# async def read_autor(
+#     autor_id: UUID,
+#     session: SessionDep,
+# ):
+#     autor = session.get(Autor, autor_id)
+#     if not autor:
+#         raise HTTPException(status_code=404, detail="Autor no encontrado")
+#     return autor
+
+# @router.post("/libros", response_model=Libro)
+# async def create_libro(
+#     libro: LibroBase,
+#     session: SessionDep
+# ):
+#     db_libro = Libro.model_validate(libro)
+#     session.add(db_libro)
+#     session.commit()
+#     session.refresh(db_libro)
+#     return db_libro
+
+# @router.get("/libros", response_model=list[Libro])
+# async def read_libros(
+#     session: SessionDep,
+#     offset: int = 0,
+#     limit: Annotated[int, Query(le=100)] = 10,
+# ):
+#     libros = session.exec(select(Libro).offset(offset).limit(limit)).all()
+#     if not libros:
+#         raise HTTPException(status_code=404, detail="No se encontraron libros")
+#     return libros
+
+# @router.get("/libros/{libro_id}", response_model=Libro)
+# async def read_libro(
+#     libro_id: UUID,
+#     session: SessionDep,
+# ):
+#     libro = session.get(Libro, libro_id)
+#     if not libro:
+#         raise HTTPException(status_code=404, detail="Libro no encontrado")
+#     return libro
