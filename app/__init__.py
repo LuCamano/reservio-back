36d761b13c74127b936944bbc8b1ca0beb75b965
Luciano <lu.camano@duocuc.cl>
2025-07-02 21:57:58 -0400

    feat: Añadir configuración de CORS para permitir orígenes específicos

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from controllers.Media import MEDIA_DIR
 from .db import create_db_and_tables
 from views import routers
+
+origins = [
+    "http://www.reservio.space",
+    "https://www.reservio.space",
+    "http://reservio.space",
+    "https://reservio.space"
+]
+
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -18,4 +28,11 @@ def create_app() -> FastAPI:
     app.mount("/media", StaticFiles(directory=MEDIA_DIR), name="media")
     for router in routers:
         app.include_router(router)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     return app
